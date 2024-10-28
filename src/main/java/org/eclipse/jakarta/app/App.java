a32da22786a3c61ec3a136708e9cc07dc80152e4
fabricionetto <agostinhonetto73@gmail.com>
2024-10-28 14:32:33 -0300

    Estudo-Maven-Java-Crud

---
+++ src/main/java/org/eclipse/jakarta/app/App.java
@@ -0,0 +1,12 @@
+package org.eclipse.jakarta.app;
+
+import org.eclipse.jakarta.jdbc.Conector;
+
+public class App {
+
+	public static void main(String[] args) {
+		Conector.conexao();
+
+	}
+
+}
---
+++ src/main/java/org/eclipse/jakarta/jdbc/Conector.java
@@ -0,0 +1,20 @@
+package org.eclipse.jakarta.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+
+	public static Connection conexao() {
+		Connection con = null;
+		try {
+			 con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bdagenda", "postgres", "1cbt89q");
+			 System.out.println("sucesso");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
